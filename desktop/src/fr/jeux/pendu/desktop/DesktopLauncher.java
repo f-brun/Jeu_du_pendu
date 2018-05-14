8b020a2e0369bf7ffca7cf3d718b00eedef3dba3
Florent <Florent@192.168.0.2>
2018-05-14 22:39:40 +0200

    Corrections build

--- desktop/src/fr/jeux/pendu/desktop/DesktopLauncher.java
+++ desktop/src/fr/jeux/pendu/desktop/DesktopLauncher.java
@@ -1,7 +1,7 @@
 package fr.jeux.pendu.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;  
 import fr.jeux.pendu.Pendu;
 
 public class DesktopLauncher {
